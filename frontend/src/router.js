a3a5d0ea3f957b74984b1ca8936bdf43f0d9215e
Will Aronow <john.aronow@gmail.com>
2019-06-13 18:08:09 -0700

    added basic app layout and routes

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Dashboard from './views/Dashboard.vue'
+import SignUp from './views/SignUp.vue'
+import About from './views/About.vue'
 
 Vue.use(Router)
 
@@ -10,16 +12,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'dashboard',
+      component: Dashboard
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignUp
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: About
     }
   ]
 })
--- frontend/src/views/SignUp.vue
+++ frontend/src/views/SignUp.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="signup">
+    <h1>Sign Up Page</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'about',
+  name: 'signup',
 }
 </script>
 
