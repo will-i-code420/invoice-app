71f54b67bdaf0e4fd61be1558536260591781136
Will Aronow <john.aronow@gmail.com>
2019-06-13 17:57:00 -0700

    added bootratp-vue for styling

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
-import App from './App.vue'
+
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import router from './router'
 
+import App from './App.vue'
+
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
