bc0342b8d46fad87a6ea652c7a74b665ec834943
Will Aronow <john.aronow@gmail.com>
2019-06-15 13:03:27 -0700

    added up function to migrations to track new additions to db

--- database/migrations/1.0.js
+++ database/migrations/1.0.js
@@ -3,3 +3,40 @@
 const Promise = require("bluebird");
 const sqlite3 = require("sqlite3");
 const path = require('path');
+
+module.exports = {
+  up: function() {
+    return new Promise(function(resolve, reject) {
+      let db = new sqlite3.Database('./database/InvoiceApp.db')
+      db.run(`PRAGMA foreign_keys = ON`);
+
+      db.serialize(function() {
+        db.run(`CREATE TABLE users (
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          email TEXT,
+          company_name TEXT,
+          password TEXT
+        )`);
+
+        db.run(`CREATE TABLE invoices (
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          user_id INTEGER,
+          paid NUMBERIC,
+          FOREIGN KEY(user_id) REFERENCES users(id)
+        )`);
+
+        db.run(`CREATE TABLE transactions (
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          price INTEGER,
+          invoice_id INTEGER,
+          FOREIGN KEY(invoice_id) REFERENCES invoices(id)
+        )`);
+      });
+      db.close();
+    });
+  },
+  
+}
