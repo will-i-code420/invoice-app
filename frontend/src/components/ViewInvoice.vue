8511b3c25bdb3ed83936645cc640c393e8cba94e
Will Aronow <john.aronow@gmail.com>
2019-06-15 10:58:34 -0700

    created view invoices page

--- frontend/src/components/ViewInvoice.vue
+++ frontend/src/components/ViewInvoice.vue
@@ -1,8 +1,58 @@
 <template>
+  <div class="view-inovices-container">
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col>
+          <h2>All Invoices:</h2>
+          <hr>
+          <table>
+            <thead>
+              <tr>
+                <th>Invoice #</th>
+                <th>Invoice Name</th>
+                <th>Current Status</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <template v-for="invoice in invoices">
+                <tr>
+                  <th>{{ invoice.id }}</th>
+                  <td>{{ invoice.name }}</td>
+                  <td v-if="invoice.paid === 0">Unpaid</td>
+                  <td v-else>Paid</td>
+                  <td>
+                    <b-button pill variant="outline-warning" size="small" to="#">
+                      View Invoice
+                    </b-button>
+                  </td>
+                </tr>
+              </template>
+            </tbody>
+          </table>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
+  data () {
+    return {
+      invoices: [],
+      user: this.$route.params.user
+    }
+  },
+  mounted () {
+    axios.get(`http://localhost:3128/invoice/user/${this.user.id}`).then(res => {
+      if (res.data.status === true) {
+        this.invoices = res.data.invoices
+      }
+    })
+  }
 }
 </script>
 
