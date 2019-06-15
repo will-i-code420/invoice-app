820a817a7fa31e789386742364c4120f866f5d96
Will Aronow <john.aronow@gmail.com>
2019-06-15 16:29:01 -0700

    added route for creating new user

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3').verbose();
+const bcrypt = require('bcrypt');
+
 const PORT = process.env.PORT || 3128;
+
+const saltRounds = 10;
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
@@ -9,6 +13,30 @@ app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   res.send("Welcome to the Invoice App");
+});
+
+app.post('/register', function(req, res) {
+  if (isEmpty(req.body.name) || isEmpty(req.body.email) || isEmpty(req.body.company_name) || isEmpty(req.body.password)) {
+    return res.json({
+      'status': false,
+      'message': 'All fields required'
+    });
+  }
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    let db = new sqlite3.Database("./database/InvoiceApp.db");
+    let sql = `INSERT INTO users(name, email, company_name, password) VALUES('${req.body.name}','${req.body.email}','${req.body.company_name}','${hash}')`;
+    db.run(sql, function(err) {
+      if (err) {
+        throw err;
+      } else {
+        return res.json({
+          status: true,
+          message: "User Created"
+        });
+      }
+    });
+    db.close()
+  });
 });
 
 app.listen(PORT, function() {
