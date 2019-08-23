874f5de638b904f6219f7db3ca3b62c7cadcf468
jaronow <waronow123@gmail.com>
2019-08-23 15:45:36 -0700

    started to make seperate sever folder to break up server.js into modules using sequelize for db management as well as modules to break up server.js into more manageable state

---
+++ server/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+      "no-unused-vars": [
+        "warn"
+      ],
+      "no-undef": [
+        "warn"
+      ]
+    }
+};
