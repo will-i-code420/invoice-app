cdc6e97f37453973e1555765c0c70877b3fdc3a9
jaronow <waronow123@gmail.com>
2019-08-26 18:13:20 -0700

    setup transaction and invoice models to create unique UUID as well

--- server/src/models/Invoice.js
+++ server/src/models/Invoice.js
@@ -1,5 +1,11 @@
 module.exports = (sequelize, DataTypes) => {
   const Invoice = sequelize.define('Invoice', {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4
+    },
     name: DataTypes.STRING,
     amount_paid: DataTypes.INTEGER,
     total_price: DataTypes.INTEGER,
--- server/src/models/Transaction.js
+++ server/src/models/Transaction.js
@@ -1,5 +1,11 @@
 module.exports = (sequelize, DataTypes) => {
   const Transaction = sequelize.define('Transaction', {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4
+    },
     item_id: DataTypes.INTEGER,
     description: DataTypes.STRING,
     quantity: DataTypes.INTEGER,
