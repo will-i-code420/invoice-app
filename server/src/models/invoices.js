5bac55fef6328c1a9be41c38e05cf2a07322ecd3
jaronow <waronow123@gmail.com>
2019-08-26 23:32:53 -0700

    updated invoice model/migrations to use UUID

--- server/src/models/invoices.js
+++ server/src/models/invoices.js
@@ -1,6 +1,12 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Invoices = sequelize.define('Invoices', {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4
+    },
     name: DataTypes.STRING,
     amount_paid: DataTypes.INTEGER,
     total_due: DataTypes.INTEGER
