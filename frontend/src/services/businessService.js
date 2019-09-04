919b2c4f284ffa29b6f78287eefa674e6a895dc3
jaronow <waronow123@gmail.com>
2019-09-04 14:37:47 -0700

    created rest of business and employee services for client side

--- frontend/src/services/businessService.js
+++ frontend/src/services/businessService.js
@@ -3,5 +3,14 @@ import api from '@/services/api'
 export default {
   index(id) {
     return api().get(`business/${id}`)
+  },
+  business(id, businessId) {
+    return api().get(`business/${id}/${businessId}`)
+  },
+  create(business) {
+    return api().post('business', business)
+  },
+  put(business) {
+    return api().put(`business/${business.id}`, business)
   }
 }
--- frontend/src/services/employeeService.js
+++ frontend/src/services/employeeService.js
@@ -3,5 +3,14 @@ import api from '@/services/api'
 export default {
   index(id) {
     return api().get(`employee/${id}`)
+  },
+  employee(id, employeeId) {
+    return api().get(`employee/${id}/${businessId}`)
+  },
+  create(employee) {
+    return api().post('employee', employee)
+  },
+  put(employee) {
+    return api().put(`invoices/${employee.id}`, employee)
   }
 }
