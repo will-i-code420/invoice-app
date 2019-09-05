6e55aef8a015aafcfe9249b508dcd3777e5d4cb7
jaronow <waronow123@gmail.com>
2019-09-05 11:56:57 -0700

    removed . from routes

--- server/src/routes.js
+++ server/src/routes.js
@@ -8,8 +8,8 @@ module.exports = (app) => {
   app.post('/register', authenticationControllerPolicy.register, authenticationController.register);
   app.post('/login', authenticationController.login);
   app.post('/invoices', invoiceController.create);
-  app.post ('./business', businessController.create);
-  app.post ('./employee', employeeController.create);
+  app.post ('/business', businessController.create);
+  app.post ('/employee', employeeController.create);
   app.put('/invoices/:id', invoiceController.put);
   app.get('/invoices/:id', invoiceController.index);
   app.get('/invoices/:id/:invoiceId', invoiceController.invoice);
