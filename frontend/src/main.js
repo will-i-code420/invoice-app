b3275e6bf2f614b6dfe37ba227d23c5f5d1fbcf0
jaronow <waronow123@gmail.com>
2019-09-16 17:42:01 -0700

    defined vue-cookies in main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,12 +3,17 @@ import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueCookies from 'vue-cookies'
 
 import router from './router'
 import { sync } from 'vuex-router-sync'
 import store from './store'
 
 Vue.use(BootstrapVue)
+Vue.use(VueCookies)
+
+VueCookies.config('1d')
+VueCookies.set('language','JavaScript')
 
 sync(store, router)
 
