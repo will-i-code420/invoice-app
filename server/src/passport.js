7a621057ef8461335f1005f98283c1ea9eb45760
jaronow <waronow123@gmail.com>
2019-09-17 13:58:18 -0700

    fixed calls to momodel and config file

--- server/src/passport.js
+++ server/src/passport.js
@@ -1,8 +1,8 @@
 const passport = require('passport');
-const {User} = require('../../models');
+const {User} = require('../models');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
-const config = require('../../config/config.json');
+const config = require('../config/config.json');
 
 passport.use(
   new JwtStrategy({
