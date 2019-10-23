628887b9c60e98829de57acd22a747da0ac3160b
jaronow <waronow123@gmail.com>
2019-10-22 17:15:34 -0700

    created bar and line charts

--- frontend/src/plugins/echarts.js
+++ frontend/src/plugins/echarts.js
@@ -2,5 +2,7 @@ import Vue from 'vue'
 import Echarts from 'vue-echarts'
 
 import 'echarts/lib/chart/bar'
+import 'echarts/lib/chart/line'
+import 'echarts/lib/component/title'
 
 Vue.component('chart', Echarts)
--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="dashboard-container">
-    <h1>Dashboard Area</h1>
-    <h2>Will Have Summary of Invoices Due, Monthly Earnings, etc</h2>
+    <div class="bar-chart">
+      <chart
+      :options="totalInvoice"
+      />
+    </div>
+    <div class="line-chart">
+      <chart
+      :options="income"
+      />
+    </div>
   </div>
 </template>
 
@@ -10,10 +18,62 @@ export default {
   name: 'dashboard',
   data () {
     return {
+      totalInvoice: {
+        title: {
+          text: 'Total Invoices',
+          x: 'center',
+          textStyle: {
+            fontSize: 28
+          }
+        },
+        color: ['#127ac2'],
+        xAxis: {
+          data: ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis: {type: 'value'},
+        series: [{
+          name: 'Total Invoices',
+          type: 'bar',
+          data: [10, 20, 30, 40]
+        }]
+      },
+      income: {
+        title: {
+          text: 'YTD Income',
+          x: 'center',
+          textStyle: {
+            fontSize: 28
+          }
+        },
+        color: ['#127ac2'],
+        xAxis: {
+          data: ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis: {type: 'value'},
+        series: [{
+          name: 'YTD Income',
+          type: 'line',
+          data: [10, 20, 30, 40]
+        }]
+      }
     }
   }
 }
 </script>
 
 <style scoped>
+
+.dashboard-container {
+  padding-top: 65px;
+}
+
+.bar-chart, .line-chart {
+  width: 100%;
+  height: 500px;
+}
+
+.echarts {
+  width: 100%;
+  height: 100%;
+}
 </style>
