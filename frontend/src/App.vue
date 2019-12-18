415e0beaf297a9e3dc374cd78ab01ace2a422270
jaronow <waronow123@gmail.com>
2019-12-18 15:04:26 -0700

    added zoom transition animation, added logic to router check to change between fade, slide, and zoom

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <transition
     :name="transitionName"
-    mode="out-in"
+    :mode="transitionMode"
+    :enter-active-class="transitionEnterActiveClass"
     >
       <router-view/>
     </transition>
@@ -11,26 +12,46 @@
 
 <script>
 const DEFAULT_TRANSITION = 'fade'
+const DEFAULT_TRANSITION_MODE = 'out-in'
 
 export default {
   name: 'App',
   data() {
     return {
-      transitionName: DEFAULT_TRANSITION
+      transitionName: DEFAULT_TRANSITION,
+      transitionMode: DEFAULT_TRANSITION_MODE,
+      transitionEnterActiveClass: ''
     }
   },
   created() {
     this.$router.beforeEach((to, from, next) => {
       // gets transition name from router.js meta tag
-      let transitionName = to.meta.transitionName || from.meta.transitionName
+      let transitionName = to.meta.transitionName || from.meta.transitionName || DEFAULT_TRANSITION
       // if transition is slide, we check if we are going deeper into the route or back a level
       if (transitionName === 'slide') {
         const toDepth = to.path.split('/').length
         const fromDepth = from.path.split('/').length
         transitionName = toDepth < fromDepth ? 'slide-right' : 'slide-left'
       }
-      // assign the transition name we are using and then going to the next action
-      this.transitionName = transitionName || DEFAULT_TRANSITION
+      // assign the transition mode we are using and active class to use
+      this.transitionMode = DEFAULT_TRANSITION_MODE
+      this.transitionEnterActiveClass = `${transitionName}-enter-active`
+      // if transition is zoom into a page
+      if (to.meta.transitionName === 'zoom') {
+        this.transitionMode = 'in-out'
+        this.transitionEnterActiveClass = 'zoom-enter-active'
+        // disable scolling in background
+        document.body.style.overflow = 'hidden'
+      }
+      // if transition is zoom from a page
+      if (from.meta.transitionName === 'zoom') {
+        this.transitionMode = null
+        this.transitionEnterActiveClass = null
+        // Enable scrolling in background
+        document.body.style.overflow = null
+      }
+      // assign transition name and call next
+      this.transitionName = transitionName
       next()
     })
   }
@@ -65,5 +86,27 @@ export default {
 .slide-right-enter {
   opacity: 0;
   transform: translate(-2em, 0);
+}
+
+.zoom-enter-active,
+.zoom-leave-active {
+  animation-duration: 0.5s;
+  animation-fill-mode: both;
+  animation-name: zoom;
+}
+
+.zoom-leave-active {
+  animation-direction: reverse;
+}
+
+@keyframes zoom {
+  from {
+    opacity: 0;
+    transform: scale3d(0.3, 0.3, 0.3);
+  }
+
+  100% {
+    opacity: 1;
+  }
 }
 </style>
