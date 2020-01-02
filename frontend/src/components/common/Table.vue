0cbaf15568dc877abfef2bc0223d7cff0c23aa35
jaronow <waronow123@gmail.com>
2020-01-01 17:51:14 -0700

    new global Table.vue and its scss

---
+++ frontend/src/components/common/Table.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="table-container">
+    <div class="table">
+      <table>
+        <thead>
+          <tr>
+            <slot name="table-head">
+            </slot>
+          </tr>
+        </thead>
+        <tbody>
+          <slot name="table-body">
+          </slot>
+        </tbody>
+        <tfoot>
+          <tr>
+            <slot name="table-foot">
+            </slot>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Table'
+}
+</script>
