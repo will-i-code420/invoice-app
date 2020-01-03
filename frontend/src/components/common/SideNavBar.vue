680300d4d37206dfc705d79457e836d456e96154
jaronow <waronow123@gmail.com>
2020-01-03 14:49:54 -0700

    created SideBarNav and blank style sheet

---
+++ frontend/src/components/common/SideNavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="side-nav-bar-container">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'side-nav-bar',
+  data() {
+    return {
+    }
+  },
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
--- frontend/src/components/common/index.js
+++ frontend/src/components/common/index.js
@@ -2,10 +2,12 @@ import FileUpload from './FileUpload.vue'
 import LogoUpload from './LogoUpload.vue'
 import Modal from './Modal.vue'
 import Table from './Table.vue'
+import SideNavBar from './SideNavBar.vue'
 
 export default [
   FileUpload,
   LogoUpload,
   Modal,
-  Table
+  Table,
+  SideNavBar
 ]
