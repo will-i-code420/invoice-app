207af2d29bc10781be35c52e7056b6e32fa53dad
jaronow <waronow123@gmail.com>
2020-01-04 12:01:54 -0700

    created sidenav template, added font-awesome to project

--- frontend/src/components/common/SideNavBar.vue
+++ frontend/src/components/common/SideNavBar.vue
@@ -1,6 +1,18 @@
 <template>
-  <div class="side-nav-bar-container">
-
+  <div class="side-nav-bar-container" :class="{'show': showSideNav}">
+    <div class="side-nav-control">
+      <fa-icon icon="['fas', 'angle-double-right']" />
+    </div>
+    <div class="side-nav-icons">
+      <slot name="icons">
+      </slot>
+    </div>
+    <div class="side-nav-links">
+      <transition-group name="side-nav-fade">
+        <slot name="links" v-if="showLinks">
+        </slot>
+      </transition-group>
+    </div>
   </div>
 </template>
 
@@ -9,11 +21,24 @@ export default {
   name: 'side-nav-bar',
   data() {
     return {
+      showSideNav: false,
+      showLinks: false
     }
   },
-  computed: {
-  },
   methods: {
+    showSideNav() {
+      if (this.showLinks) {
+        this.showLinks = false
+        setTimeout(() => {
+          this.showSideNav = false
+        }, 500)
+      } else {
+        this.showSideNav = true
+        setTimeout(() => {
+          this.showLinks = true
+        },500)
+      }
+    }
   }
 }
 </script>
